175a56c40b0f88bc7b7b2a9f382b5fbe7dd0aca2
k0skar <koskarnoza@gmail.com>
2020-01-31 04:17:26 +0200

    initial commit

---
+++ precache-manifest.0c53ed46a2c1f62a0afc80a9931e56e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8380afdbdb6c36f2ba1f1acf370efcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7a0b232d80c2b4a6b87",
+    "url": "/static/css/main.7741bd19.chunk.css"
+  },
+  {
+    "revision": "c77ed005940ecaaae530",
+    "url": "/static/js/2.7c45398a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7c45398a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7a0b232d80c2b4a6b87",
+    "url": "/static/js/main.917e4e93.chunk.js"
+  },
+  {
+    "revision": "2a9a4f373d7fb2da8851",
+    "url": "/static/js/runtime-main.5172e516.js"
+  }
+]);
